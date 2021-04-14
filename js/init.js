abfa36fab1634270a8e6fb8a613b6fa8c3b56b75
divisionCWDB <74282980+divisionCWDB@users.noreply.github.com>
2021-04-13 21:18:54 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBTHnD-RQI-sgu9XqH9XyNZfi0e5SX96ng",
+  authDomain: "controlador-iot.firebaseapp.com",
+  projectId: "controlador-iot",
+  storageBucket: "controlador-iot.appspot.com",
+  messagingSenderId: "459085222631",
+  appId: "1:459085222631:web:2a11534f1662db567f8842",
+  measurementId: "G-0VZ62QY50N"
+});
