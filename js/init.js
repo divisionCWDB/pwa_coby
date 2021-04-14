68008f873a44007472bd45c4da4302c4517ae7b5
divisionCWDB <74282980+divisionCWDB@users.noreply.github.com>
2021-04-13 22:14:39 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 // @ts-ignore
 firebase.initializeApp({
    apiKey: "AIzaSyBNYdBXbTk7Q6jv1G2SyePZt2IhxATGinI",
-    authDomain: "pwa-coby.firebaseapp.com",
-    projectId: "pwa-coby",
-    storageBucket: "pwa-coby.appspot.com",
-    messagingSenderId: "824413379959",
-    appId: "1:824413379959:web:87efcbc7f150293ec65572",
-    measurementId: "G-KJF0H1P1E8"
+  authDomain: "pwa-coby.firebaseapp.com",
+  projectId: "pwa-coby",
+  storageBucket: "pwa-coby.appspot.com",
+  messagingSenderId: "824413379959",
+  appId: "1:824413379959:web:87efcbc7f150293ec65572",
+  measurementId: "G-KJF0H1P1E8"
 });
